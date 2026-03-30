85ff255e2f45f6e289dc0b4b250c74d7d28a0319
Juan Delgado <dsikorp@gmail.com>
2026-03-30 13:49:02 -0500

    Order services ready

---
+++ order-service/src/main/java/com/ecommerce/order_service/exception/GlobalControllerAdvice.java
@@ -0,0 +1,80 @@
+package com.ecommerce.order_service.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.net.URI;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+@Slf4j // log
+public class GlobalControllerAdvice {
+
+        @ExceptionHandler(ResourceNotFoundException.class)
+        public ProblemDetail handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+
+                log.warn("Resource not found - Path: {}, Message: {}",
+                                request.getDescription(false), ex.getMessage());
+
+                ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+
+                problemDetail.setTitle("Resource not found");
+                problemDetail.setType(URI.create("https://api.ecommerce.com/errors/not-found"));
+                problemDetail.setProperty("Timestamp", Instant.now());
+
+                problemDetail.setProperty("Resource", ex.getResourceName());
+                problemDetail.setProperty("Field", ex.getFieldName());
+                problemDetail.setProperty("Value", ex.getFieldValue());
+
+                return problemDetail;
+        }
+
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+
+                ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,
+                                "Validation failed in one or more fields");
+
+                problemDetail.setTitle("Validation error");
+                problemDetail.setType(URI.create("https://api.ecommerce.com/errors/error-validation"));
+                problemDetail.setProperty("Timestamp", Instant.now());
+
+                Map<String, String> errorMap = new HashMap<>();
+
+                ex.getBindingResult().getFieldErrors().forEach(
+                                error -> {
+                                        errorMap.put(error.getField(), error.getDefaultMessage());
+                                });
+
+                problemDetail.setProperty("errors", errorMap);
+
+                log.error("Validation error: \n{}  \n{}",
+                                errorMap, ex.getMessage(), ex);
+
+                return problemDetail;
+        }
+
+        @ExceptionHandler(Exception.class)
+        public ProblemDetail handleException(Exception ex, WebRequest request) {
+
+                log.error("An unexpected error has occurred {}:  {}",
+                                request.getDescription(false), ex.getMessage(), ex);
+
+                ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR,
+                                "An unexpected error has occurred. Please contact the administrator.");
+
+                problemDetail.setTitle("Internal Server Error");
+                problemDetail.setType(URI.create("https://api.ecommerce.com/errors/internal"));
+                problemDetail.setProperty("Timestamp", Instant.now());
+
+                return problemDetail;
+
+        }
+}
