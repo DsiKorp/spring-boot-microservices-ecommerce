358a4260ce7ddc28289740871b8b5b857b410733
Juan Delgado <dsikorp@gmail.com>
2026-04-01 19:09:34 -0500

    product-service dinamic ip and port

--- order-service/src/main/java/com/ecommerce/order_service/config/WebClientConfig.java
+++ order-service/src/main/java/com/ecommerce/order_service/config/WebClientConfig.java
@@ -2,6 +2,7 @@ package com.ecommerce.order_service.config;
 
 import com.ecommerce.order_service.service.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -14,18 +15,27 @@ public class WebClientConfig {
     @Value("${inventory.service.url}")
     private String inventoryServiceUrl;
 
-//    @Bean
-//    public WebClient.Builder webClientBuilder() {
-//        return WebClient.builder();
-//    }
 
-    @Bean
+/*
+    para ip y puerto fijo del .evv
+   @Bean
     public WebClient webClientBuilder() {
         return WebClient.builder().baseUrl(inventoryServiceUrl).build();
     }
+*/
+
+    // para eureka ip y puerto dinámico
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
 
     @Bean
-    InventoryClient inventoryClient(WebClient webClient) {
+    InventoryClient inventoryClient(WebClient.Builder builder) {
+        WebClient webClient = builder.baseUrl(inventoryServiceUrl).build();
+
+
         // crear un adaptador para poder usar WebClient como cliente de servicio
         // WebClientAdapter es el que sabe como ejecutar la petición utilizando webClient
         WebClientAdapter adapter = WebClientAdapter.create(webClient);
