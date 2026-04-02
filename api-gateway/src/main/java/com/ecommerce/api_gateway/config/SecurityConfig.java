0d6cae75597a89981023f00e19afa3fe59443feb
Juan Delgado <dsikorp@gmail.com>
2026-04-02 12:20:20 -0500

    KeyCloak security Realm config api-gateway yml GatewayConfig

--- api-gateway/src/main/java/com/ecommerce/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/ecommerce/api_gateway/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.ecommerce.order_service.config;
+package com.ecommerce.api_gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
