70a1baaa082cab98f562f56a9f8a52d013a2ef99
Juan Delgado <dsikorp@gmail.com>
2026-04-03 12:40:29 -0500

    RabbitMq Config order notification inventory service

---
+++ inventory-service/src/main/java/com/ecommerce/inventory_service/config/RabbitMQConfig.java
@@ -0,0 +1,17 @@
+package com.ecommerce.inventory_service.config;
+
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    // En vez de utilizar serialización java, se utiliza Jackson para convertir objetos a JSON
+    @Bean
+    public MessageConverter messageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+}
---
+++ notification-service/src/main/java/config/RabbitMQConfig.java
@@ -0,0 +1,17 @@
+package config;
+
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    // En vez de utilizar serialización java, se utiliza Jackson para convertir objetos a JSON
+    @Bean
+    public MessageConverter messageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+}
