cda8c5589696682fe73e598be026513e53cf54c0
Juan Delgado <dsikorp@gmail.com>
2026-04-03 17:06:14 -0500

    RabbitMq OrderListener into inventory service

--- notification-service/src/main/java/com/ecommerce/notification_service/config/RabbitMQConfig.java
+++ notification-service/src/main/java/com/ecommerce/notification_service/config/RabbitMQConfig.java
@@ -8,9 +8,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
-    // En vez de utilizar serialización java, se utiliza Jackson para convertir objetos a JSON
+    // En vez de utilizar serialización java, se utiliza Jackson
+    // para convertir objetos a JSON
     @Bean
     public MessageConverter messageConverter() {
+
         return new JacksonJsonMessageConverter();
     }
 
