32d590e391028ea237644a90a5d347fad26511e9
Juan Delgado <dsikorp@gmail.com>
2026-04-03 19:52:23 -0500

    config mail trap

--- notification-service/src/main/java/com/ecommerce/notification_service/listener/OrderEventsListener.java
+++ notification-service/src/main/java/com/ecommerce/notification_service/listener/OrderEventsListener.java
@@ -1,13 +1,20 @@
 package com.ecommerce.notification_service.listener;
 
 import com.ecommerce.notification_service.event.OrderPlacedEvent;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.mail.SimpleMailMessage;
+
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class OrderEventsListener {
+
+    private final JavaMailSender mailSender;
 
     @RabbitListener(queues = "notification-queue")
     public void handleOrderPlacedEvent(OrderPlacedEvent orderPlacedEvent) {
@@ -16,7 +23,16 @@ public class OrderEventsListener {
 
         orderPlacedEvent.items().forEach( item -> {
             try {
+
+                SimpleMailMessage message = new SimpleMailMessage();
+                message.setFrom("orders@dsklabs.com");
+                message.setTo(orderPlacedEvent.email());
+                message.setSubject("Order Confirmation: " + orderPlacedEvent.orderNumber());
+                message.setText("Thank you for your order! \nYour order number is: " + orderPlacedEvent.orderNumber());
+                mailSender.send(message);
+
                  log.info("✅\u200B Sending confirm email to: {}, Order {}", orderPlacedEvent.email(), orderPlacedEvent.orderNumber());
+
 
                  log.info("✅\u200B email to: {}, was sent successfully", orderPlacedEvent.email());
             } catch (Exception e) {
